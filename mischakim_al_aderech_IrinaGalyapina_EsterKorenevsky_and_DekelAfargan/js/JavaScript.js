4483adb36e34465cde5dbfed402f0b8bcbc3edfe
Ira Galyapina <irengalyapina@gmail.com>
2017-07-04 02:44:35 +0300

    Add files via upload

---
+++ mischakim_al_aderech_IrinaGalyapina_EsterKorenevsky_and_DekelAfargan/js/JavaScript.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('.accordion').find('.accordion-toggle').click(function () {
+        $(this).next().slideToggle('600');
+        $(".accordion-content").not($(this).next()).slideUp('600');
+    });
+    $('.accordion-toggle').on('click', function () {
+        // $(this).toggleClass('active').parent().siblings().children('h3').removeClass('active');
+        $('.accordion-toggle').removeClass('active');
+        $(this).toggleClass('active');
+    });
+});
